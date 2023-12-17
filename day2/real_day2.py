fdb313135fff4c61f324b742729a6f5fbabe65e5
Gunhee <deocode001@gmail.com>
2023-12-17 03:17:22 -0500

    day 2 part 1

--- day2/real_day2.py
+++ day2/real_day2.py
@@ -1,5 +1,5 @@
 import re
-with open('practice_d2p1.txt', 'r') as f:
+with open('real_day2.txt', 'r') as f:
     puzzles = f.read()
 def part1(puzzles):
 
@@ -9,26 +9,28 @@ def part1(puzzles):
     
     #grouped = sectioned_list.split(";")
     #print(grouped)
-    possibleid = False
+    
     for id, game in enumerate(sectioned_list, start=1):
+        possibleid = True
         game_split = game.split(': ')
         for str_of_group in game_split[1:]:
             pairs = str_of_group.split('; ')
             for pair in pairs:
-    
                 finalpair = pair.split(', ')
-                print(finalpair)
                 for kv in finalpair:
-                    number, colour = kv.split()
-                    if int(number) >= conditions[colour]:
-                        print(number)
-                        print(conditions[colour])
-                        possibleid == True
                     
-        if possibleid == True:
-            sum_of_game_ids+=id
-
+                    
+                    number, colour = kv.split()
+                    if int(number) > conditions[colour]:
+                        possibleid = False
                         
+                    
+
+            if possibleid is True:
+                sum_of_game_ids+=id
+        
+
+                     
     return sum_of_game_ids
         
     
